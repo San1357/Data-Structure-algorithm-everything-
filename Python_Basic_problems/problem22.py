0f2af9bd7345ed6e36c630eb3e10e375f58399af
San1357 <san1357@gmail.com>
2021-09-22 17:02:23 +0530

    simple interset prblem

---
+++ Python_Basic_problems/problem22.py
@@ -0,0 +1,6 @@
+#program to compute simple interest Given all required values.
+principle = int(input("enter the prainciple value:"))
+rate = int(input("enter the rate value:"))
+time = int(input("enter the time:"))
+S_I = (principle * rate * time)/100
+print (S_I)
