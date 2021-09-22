d2396a6b91aff60c654e5b9fbaf2c8ab68616a3b
San1357 <san1357@gmail.com>
2021-09-22 19:11:55 +0530

    prime factors using while loop

--- Python_Basic_problems/problem26.py
+++ Python_Basic_problems/problem26.py
@@ -14,3 +14,24 @@ def factor(n):
   print ("primefactors is :",primefactors)
 
 factor(98)
+
+        # or code 2:
+
+# this code using while loop 
+'''
+n=int(input("Enter an integer:"))
+print("Factors are:")
+i=1
+while(i<=n):
+    k=0
+    if(n%i==0):
+        j=1
+        while(j<=i):
+            if(i%j==0):
+                k=k+1
+            j=j+1
+        if(k==2):
+            print(i)
+    i=i+1
+
+'''
