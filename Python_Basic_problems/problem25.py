483dfa6b47b73d8f86c3d248b82fb7531cbf0ccb
San1357 <san1357@gmail.com>
2021-09-22 18:01:42 +0530

    temperature celcius to fahrenheit

---
+++ Python_Basic_problems/problem25.py
@@ -0,0 +1,4 @@
+# program to take temperature in celcius & convert it into farhenheit
+temperature = float(input("enter the temperature in celcius:"))
+farhenheit = (temperature * 1.8)+32
+print("your temperature from celcius to farhenheit is:", farhenheit, ""+ "Fahrenheit")
