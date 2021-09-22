675d34c40bf033b7115f9e22e13714cd403d7590
San1357 <san1357@gmail.com>
2021-09-22 19:21:11 +0530

    problem27.py

--- Python_Basic_problems/problem27.py
+++ Python_Basic_problems/problem27.py
@@ -1,6 +1,9 @@
 # Program to Generate all the Divisors of an Integer 
 
 n = int(input("enter any number:"))
+divisor = [] 
 for i in range(1, n+1):
     if n % i == 0:
-        print(i)
+        divisor.append(i)
+
+print("The divisor of the no.", n, "are:", divisor)
