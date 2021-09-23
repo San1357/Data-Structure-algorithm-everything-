01bfb38492cd1cbbb7693cdbfbdce79f7c1c73ea
San1357 <san1357@gmail.com>
2021-09-23 12:00:57 +0530

    using while loop too

--- Python_Basic_problems/problem31.py
+++ Python_Basic_problems/problem31.py
@@ -5,3 +5,20 @@ number = input("enter the number:")
 for i in number:
     if i == number[-1]:
         print(str(len(number))+str(i))
+
+        # OR code 2nd
+
+    #using while loop
+'''
+n=int(input("Enter the number:"))
+tmp=n
+k=0
+while(n>0):
+    k=k+1
+    n=n//10
+b=str(tmp)
+c=str(k)
+d=c+b[k-1]
+print("The new number formed:",int(d))
+'''
+        
